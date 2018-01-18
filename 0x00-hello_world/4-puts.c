3484d0bcf1278450cb4e912ae726fc88f725fc93
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-18 01:02:23 +0000

    Prints string and return 0 if successful

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - prints out a string
+ *
+ * Return: Always 0 if successful
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
