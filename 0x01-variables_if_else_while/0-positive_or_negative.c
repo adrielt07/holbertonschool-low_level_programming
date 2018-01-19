b53a0ccdded59c79c4f105dd20c11966f84d5a98
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 00:02:28 +0000

    identify if random number is negative, positive or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints random value and identify if it's zero, positive or negative
+ * Return: 0 if suecessful
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+		printf("%d is zero", n);
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+
+	return (0);
+}
