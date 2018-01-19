14dca6496e78638c4b2a5db0ff36267c25cf9520
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 05:06:02 +0000

    used while command to print a-z and A-Z

---
+++ 0x01-variables_if_else_while/3-print-alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print a - z and A - Z
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+
+}
