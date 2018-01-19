236f3c9afee87e66d2efb6f8b3fd1513aa02316c
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 05:42:56 +0000

    print hexidecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints hexidecimal
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char n = 'a';
+	char i = '0';
+	
+	while(i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	while(n < 'g')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
