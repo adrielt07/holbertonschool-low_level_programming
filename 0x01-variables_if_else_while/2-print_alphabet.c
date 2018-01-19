484810b625a77db1c03570478a6a10b1d88b6b94
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 01:37:00 +0000

    use for loop to print the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet a - z
+ * Return: 0 is successful
+ */
+
+int main(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+	return (0);
+}
