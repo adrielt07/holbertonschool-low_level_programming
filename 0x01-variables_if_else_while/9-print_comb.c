17cf779fb73808e63ea1e61c4db3a89d34ccc323
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 05:52:19 +0000

    use while with if

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints 1 - 9 seprated by space
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char n = '0';
+	
+	while (n <= '9')
+	{
+	putchar(n);
+		if (n < '9')
+			putchar(',');
+			putchar(' ');
+	n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
