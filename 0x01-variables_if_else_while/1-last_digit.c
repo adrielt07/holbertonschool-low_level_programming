f2816318a25adc85eafad82601d102d6f743a6ef
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-19 02:58:18 +0000

    removed the variable declaration

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,24 +12,22 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	int ld = n % 10;
-
-	if (ld == 0)
+	if (n % 10 == 0)
 	{
 		printf("Last digit of %d ", n);
-		printf("is %d ", ld);
+		printf("is %d ", n % 10);
 		printf("and is 0\n");
 	}
-	else if (ld > 5)
+	else if (n % 10 > 5)
 	{
 		printf("Last digit of %d ", n);
-		printf("is %d ", ld);
+		printf("is %d ", n % 10);
 		printf("and is greater than 5\n");
 	}
 	else
 	{
 		printf("Last digit of %d ", n);
-		printf("is %d ", ld);
+		printf("is %d ", n % 10);
 		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
