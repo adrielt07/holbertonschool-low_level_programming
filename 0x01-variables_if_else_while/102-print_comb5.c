82b53b39e0909d6d96f59cb8b42d07783c8adb47
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-20 00:41:33 +0000

    fix betty styling

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 /**
- * main - prints combination of two two digits 
+ * main - prints combination of two two digits
  * but 2nd digits are greater than the first
  * Return: 0 if successful
  */
-int main(void) 
-{
-  int a, b, c, d;
 
-  for(a = '0'; a <= '9'; a++)
-  {
-    for(b = '0'; b <= '9'; b++)
-    {
-      for(c = '0'; c <= '9'; c++)
-      {
-        for(d = '0'; d <= '9'; d++)
-        {
-          if (c >= a && d > b)
-          {
-          putchar(a);
-          putchar(b);
-          putchar(' ');
-          putchar(c);
-          putchar(d);
-          putchar(',');
-          putchar(' ');
-          }
-        }
-      }
-    }
-  }
-  putchar('\n');
-  return 0;
+int main(void)
+{
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			for (d = '0'; d <= '9'; d++)
+			{
+			if (c >= a && d > b)
+			{
+			putchar(a);
+			putchar(b);
+			putchar(' ');
+			putchar(c);
+			putchar(d);
+			putchar(',');
+			putchar(' ');
+			}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
 }
