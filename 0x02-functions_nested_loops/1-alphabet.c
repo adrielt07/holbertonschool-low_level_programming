b9d2b2f492cd0bc697a295fc352aa8e61b8504c0
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-23 18:10:26 +0000

    added comment

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 /**
+ * main - call holberton header to use 
+ * print_alphabet function to print
+ * alphabet from a to z
  *
+ * Return: 0 if successful
  */
 
 int main(void)
