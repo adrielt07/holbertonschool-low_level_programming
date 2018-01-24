7cd67bae620647f5afbae0cba4d9cb380c3e25b5
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-24 14:33:01 +0000

    edited comment for betty doc

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
 
 /**
+ * add - adds two intergers
+ * @n: first interger parameter
+ * @i: second interger parameter
  *
+ * Return: adds n and i
  */
 
 int add(int n, int i)
 {
-  return (n + i);
+return (n + i);
 }
