86d8aa4ca43d2c8aa9573fde61459e6fb2f0c692
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-24 00:14:54 +0000

    edit file to only include function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,10 +3,17 @@
 /**
  * main - check the code for Holberton School students.
  *
- * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	int n;
+	int i;
+	for (i = 0; i < 10; i++) 
+	{
+        for (n = 'a'; n <= 'z'; n++)
+        {
+                _putchar(n);
+        }
+        _putchar('\n');
+	}
 }
