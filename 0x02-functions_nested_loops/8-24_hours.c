0b45f770f70ae73073b24730d2ba90e6fd7d6356
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-24 08:11:00 +0000

    added an if statement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,13 @@ int d;
 
 	for (a = '0'; a <= '2'; a++)
 	{
-		for (b = '0'; b <= '3'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '5'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
+				{
+				if ((a <= '1' && b <= '9') || (a == '2' && b <= '3'))
 				{
 				_putchar(a);
 				_putchar(b);
@@ -25,6 +27,7 @@ int d;
 				_putchar(c);
 				_putchar(d);
 				_putchar('\n');
+				}
 				}
 			}
 		}
