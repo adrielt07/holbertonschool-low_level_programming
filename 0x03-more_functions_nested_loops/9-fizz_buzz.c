a4d4cdb0fa9e1015ac1eae7fddd2b3d8809c11c0
adrielt07 <adriel_tolentino@outlook.com>
2018-01-25 22:09:15 +0000

    replace multiples of 3 and 5 with fizz buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+/**
+ * main - replace multiples of 3 and 5
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+	int j;
+	int k;
+
+	for (n = 1; n <= 100; n++)
+	{
+		j = n % 3;
+		k = n % 5;
+
+		if (j == 0 && k == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (j == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (k == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", n);
+		}
+	}
+	return (0);
+
+}
