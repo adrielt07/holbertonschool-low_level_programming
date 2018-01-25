4dc9c005c6c405f615c207e753f05fa8a2923d40
adrielt07 <adriel_tolentino@outlook.com>
2018-01-25 17:27:39 +0000

    added header

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,26 @@
+#ifndef _HOLBERTON2_
+#define _HOLBERTON2_
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+oid print_triangle(int size);
+
+#endif
