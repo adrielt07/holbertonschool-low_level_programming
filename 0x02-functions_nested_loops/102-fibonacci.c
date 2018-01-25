7449073e7108a502542af973815a5dd6398fd81b
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-25 00:43:30 +0000

    use loop to print fibonical sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+/**
+ * main - list fibonical numbers up to 50
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	unsigned long int a;
+	unsigned long int b;
+	unsigned long int c;
+	unsigned long int sum;
+	b = 0;
+	c = 1;
+	sum = 1;
+
+	for (a = 0; a < 50; a++)
+	{
+		sum = b + c;
+		b = c;
+		c = sum;
+		printf ("%lu", sum);
+		if(a < 49)
+		{
+			printf (", ");
+		}
+	}
+	return 0;
+}
