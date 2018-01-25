70a5bd113892b961ccaaf752ba496f4b62bf0669
adrielt07 <adriel_tolentino@outlook.com>
2018-01-25 18:03:16 +0000

    prints 0 - 9 except 2 and 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print 0 - 9
+ * but don't print 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		if (j != '2' && j != '4')
+		_putchar(j);
+       	}
+	_putchar('\n');
+}
