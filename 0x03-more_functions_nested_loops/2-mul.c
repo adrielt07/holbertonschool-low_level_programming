baef78b3b92164dd6f30fffef389be92d11bb156
adrielt07 <adriel_tolentino@outlook.com>
2018-01-25 17:44:26 +0000

    program multiplies two int

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two int
+ *
+ * @a: takes 1st int
+ * @b: takes 2nd int
+ *
+ * Return: return multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
