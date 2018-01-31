6bb910a3e31e00b961bc6dc76ba4f701f5ddfcad
Adriel Tolentino <adrieltolentino@outlook.com>
2018-01-31 05:07:02 +0000

    fixed the indentation for betty style

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints strings followed by a new line
+ * @str: parameter for string
+ */
+
+void _puts(char *str)
+{
+
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+		_putchar('\n');
+	}
+}
