6c75a3f12087620a9b5a72f7854eecc524c5552c
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-02 06:47:00 +0000

    first push

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: takes first string
+ * @s2: takes second string
+ * Return: Difference between two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int k;
+
+	for (k = 0; k != '\0'; k++)
+	{
+		;
+	}
+	return (s1[k] - s2[k]);
+}
