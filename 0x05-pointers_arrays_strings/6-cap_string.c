0c14a1d424ab8c600a519da8bbd346ed7b9c8a4c
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-02 13:10:45 +0000

    added char pointer to special characters and loop it until it equals in the string in if statement

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,17 @@
 char *cap_string(char *p)
 {
 	int n;
-	int k;
+	int i;
+	char *no = " ,;.!?\"(){}\t\n";
 
 	for (n = 0; p[n] != '\0'; n++)
 	{
-		if(p[n] < 'A' || p[n] > 'Z' && p[n] < 'a' || p[n] > 'z')
+		for (i = 0; no[i] != '\0'; i++)
 		{
-			p[n + 1] = p[n + 1] - 32;
+			if (p[n - 1] == no[i] && (p[n] >= 'a' && p[n] <= 'z'))
+			{
+				p[n] = p[n] - 32;
+			}
 		}
 	}
 	return (p);
