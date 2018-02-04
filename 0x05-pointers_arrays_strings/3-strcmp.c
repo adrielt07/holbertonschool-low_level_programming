7f5d620c97027031badacbbcbdea319f59fe812d
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-04 00:17:36 +0000

    fix for loop so it check further than the first character

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int k;
-
-	for (k = 0; k != '\0'; k++)
 	{
-		;
+		int k;
+
+		for (k = 0; s1[k] != '\0' || s2[k] != '\0'; k++)
+		{
+			if (s1[k] != s2[k])
+				return (s1[k] - s2[k]);
+		}
+		return (0);
 	}
-	return (s1[k] - s2[k]);
 }
