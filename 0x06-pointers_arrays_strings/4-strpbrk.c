9e621176285605fae477850c205736268fa5786e
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-06 05:02:36 +0000

    first push

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - searches [accept] in a string and prints whole string from it
+ * @s: takes a string
+ * @accept: characters to search in a string
+ * Return: strings or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int n;
+	int i;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[n] == accept[i])
+				return (s + n);
+		}
+	}
+	return ("");
+}
