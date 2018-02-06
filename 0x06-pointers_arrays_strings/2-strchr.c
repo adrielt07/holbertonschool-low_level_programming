becaa25fcfd8b1f0a9f47002605a62bcde4c0683
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-06 02:14:36 +0000

    change from pointer notation to regular notation

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0'; s++)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
-			return ((char *)s);
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
 	}
 	return ("");
 }
