2343f884d5939ca85e2f0732d9a5b1b8d30703b8
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-07 00:10:59 +0000

    print chess board

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int r;
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (r = 0; r < 8; r++)
+		{
+			_putchar(a[c][r]);
+		}
+		_putchar('\n');
+	}
+}
