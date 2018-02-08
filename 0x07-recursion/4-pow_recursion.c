9686f3a8cd60237a767fe53d3541abcf0ec24a92
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-08 04:04:37 +0000

    first push

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - calculate power of int x
+ * @x: number to multiply by itself depending on number of y
+ * @y: number of times to multiply
+ * Return: -1 if y < 0; return 1 if y = 0; else return power number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y >= 1)
+		return (x * _pow_recursion(x, y - 1));
+}
