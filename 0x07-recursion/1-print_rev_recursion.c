23f0a592ecc08e7740e6922d2360f3e180d359bd
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-08 02:17:03 +0000

    first push

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: takes string
+ */
+
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
