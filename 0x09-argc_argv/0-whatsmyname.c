ff50f0274a53b46a8883183de0d7fe84c311f48b
adrielt07 <adriel_tolentino@outlook.com>
2018-02-11 05:23:12 +0000

    first push

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - prints file name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
