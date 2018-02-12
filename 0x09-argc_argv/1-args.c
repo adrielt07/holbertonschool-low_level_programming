14805d0da82fd2a795809459b7b93251b81a2bed
adrielt07 <adriel_tolentino@outlook.com>
2018-02-12 02:57:33 +0000

    first push

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * main - prints how many arguments are passed
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+
+	int n = 0;
+
+	if (argv[n > 1])
+		printf("%d\n", argc - 1);
+	return (0);
+
+}
