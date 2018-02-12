7ef119e92da8271735df3449a4c4be28fe74e486
adrielt07 <adriel_tolentino@outlook.com>
2018-02-12 03:06:58 +0000

    first push

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * main - prints arguments per line
+ * @argc: argument count
+ * @argv: argument ventor
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	int n;
+
+	for (n = 0; n <= argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
