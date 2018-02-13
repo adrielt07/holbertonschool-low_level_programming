670810752a6197d48b7ad8c3d61cc1b604e564e3
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-13 00:51:31 +0000

    change while condition to >=

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -12,44 +12,45 @@ int main(int argc, char **argv)
 	int n = atoi(argv[1]);
 	int c = 0;
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-	}
-
 	if (n < 0)
 	{
-		printf("0\n");
-		return(0);
+		printf ("0\n");
 	}
 
-	if (argc == 2)
+	else if (argc == 2)
 	{
-		while (n / 25)
+		while (n >= 25)
 		{
 			c++;
 			n -= 25;
 		}
-		while (n / 10)
+		while (n >= 10)
 		{
 			c++;
 			n -= 10;
 		}
-		while (n / 5)
+		while (n >= 5)
 		{
 			c++;
-			n -= 10;
+			n -= 5;
 		}
-		while (n / 2)
+		while (n >= 2)
 		{
 			c++;
 			n -= 2;
 		}
-		while (n / 1)
+		while (n >= 1)
 		{
 			c++;
 			n -= 1;
 		}
 	}
+
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	printf("%d\n", c);
 }
