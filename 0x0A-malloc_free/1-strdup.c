df14afedf8ce4ae890d9589eff65cff0edc981cb
adrielt07 <adriel_tolentino@outlook.com>
2018-02-14 04:04:03 +0000

    prints string

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ * _strlen - calculate the length of string
+ * @s: string to be calculated
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+
+/**
+ * _strdup - prints string
+ * @str: string to be printed
+ * Return: Null of no string else return string
+ */
+
+char *_strdup(char *str)
+{
+	int l = _strlen(str);
+	char *s_ptr;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		s_ptr = malloc(sizeof(char) * l);
+	}
+
+	if (s_ptr == NULL)
+		return (NULL);
+
+	while (i <= l)
+	{
+		s_ptr[i] = str[i];
+		i++;
+	}
+	return (s_ptr);
+}
