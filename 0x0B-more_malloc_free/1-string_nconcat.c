27e308c56fca8f3de7b4ff4047c0a4f8decdb2e5
adrielt07 <adriel_tolentino@outlook.com>
2018-02-16 05:46:05 +0000

    changed if n >= l2 to n > l2

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -39,9 +39,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n >= l2)
+	if (n > l2)
 	{
 		n = l2;
+	}
+	else
+	{
+		l2 = n;
 	}
 
 	t = _strlen(s1) + n;
