f4f3885cc1752ec39e5e94a11b668002bc678e49
adrielt07 <adriel_tolentino@outlook.com>
2018-02-16 06:13:46 +0000

    first push

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ * _calloc - allocate mem of an array
+ * @nmemb: number of elements array
+ * @size: size of data
+ * Return: NULL if arg is 0 else return value of p
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+
+
+	while (c < nmemb)
+	{
+		p[c] = 0;
+		c++;
+	}
+	return (p);
+}
