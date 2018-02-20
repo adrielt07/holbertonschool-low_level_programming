c7aa7da52116352d56ed9f8d5b24174be5a7f39a
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-20 00:06:28 +0000

    first push

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog
+#define _dog
+
+/**
+ * struct dog - structure for dog name age and owner
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
