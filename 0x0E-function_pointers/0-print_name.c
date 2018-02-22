7f6b14d8e397c28dba64f4eabdf0533fababa77f
adrielt07 <adriel_tolentino@outlook.com>
2018-02-22 03:42:02 +0000

    first push

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: take the name
+ * @f: pointer to print_name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
