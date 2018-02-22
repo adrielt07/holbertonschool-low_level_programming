cba9f56ed8fac68ddbcd155c4795f32b14bb7f27
adrielt07 <adriel_tolentino@outlook.com>
2018-02-22 04:23:56 +0000

    fix documentation

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - prints name
  * @name: take the name
- * @f: pointer to print_name
+ * @f: pointer to function with one arg char *
  */
 
 void print_name(char *name, void (*f)(char *))
