399768d31a66643d1e8f4bc1fb2fa77b73e29c0d
Adriel Tolentino <adrieltolentino@outlook.com>
2018-02-22 22:52:41 +0000

    new one missed alot of checked - reverting to previous version

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -21,11 +21,5 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	printf("%d\n", get_op_func(argv[2])(a, b));
 }
