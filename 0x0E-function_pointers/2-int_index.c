2cdd4170c74362aee751417be7368ac6eeb7517e
adrielt07 <adriel_tolentino@outlook.com>
2018-02-22 05:00:11 +0000

    first push

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "stddef.h"
+#include "function_pointers.h"
+
+/**
+ * int_index - compare array intergers
+ * @array: takes an array
+ * @size: size of an array
+ * @cmp: function pointer to what needs to be compared to
+ * Return: first position of element. Or -1 if error
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int c;
+
+	if (size <= 0)
+		return (-1);
+
+
+	if (array && cmp)
+	{
+		for (c = 0; array[c] < size; c++)
+		{
+			if (cmp(array[c]))
+				break;
+		}
+		return (c);
+	}
+	return (-1);
+}
