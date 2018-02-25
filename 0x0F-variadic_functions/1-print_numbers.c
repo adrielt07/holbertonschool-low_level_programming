d3d7f6fb049904b27251d1785edef36f8b881a19
adrielt07 <adriel_tolentino@outlook.com>
2018-02-25 04:42:35 +0000

    first push

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * print_numbers - prints number followed by new line
+ * @separator: separates each number
+ * @n: number of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list valist;
+
+	va_start(valist, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(valist, int));
+			if (i != n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+	}
+}
