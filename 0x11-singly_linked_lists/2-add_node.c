569c587d4311c7e3548a5f5d77085bf2a8a90a10
adrielt07 <adriel_tolentino@outlook.com>
2018-03-02 05:01:09 +0000

    first push

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include "string.h"
+#include "stdlib.h"
+
+/**
+ * _strlen - calculate the length of string
+ * @s: string to be calculated
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+        int l = 0;
+
+        while (s[l] != '\0')
+        {
+                l++;
+        }
+        return (l);
+}
+
+/**
+ * add_node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newN;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	newN = malloc(sizeof(list_t));
+	if (newN == NULL)
+		return (NULL);
+
+	newN->str = strdup(str);
+	newN->len = _strlen(str);
+	newN->next = *head;
+	*head = newN;
+	return (newN);
+}
