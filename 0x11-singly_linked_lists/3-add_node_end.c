3403e0e8150f2f71f01065b0a84459267c23c928
adrielt07 <adriel_tolentino@outlook.com>
2018-03-02 06:06:48 +0000

    first push

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include "string.h"
+#include "stdlib.h"
+
+/**
+ * _strlen - calculate the length of string
+ * @s: string to be calculated
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
+
+/**
+ * add_node_end - add node at the end
+ * @head: new node to be created
+ * @str: value of new node
+ * Return: new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newN;
+
+	if (str == NULL)
+		return (NULL);
+
+	newN = malloc(sizeof(list_t));
+	if (newN == NULL)
+		return (NULL);
+
+	newN->str = strdup(str);
+	newN->len = _strlen(str);
+	newN->next = NULL;
+	*head = newN;
+
+	return (newN);
+
+}
