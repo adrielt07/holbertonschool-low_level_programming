1c0b2e60099cab652c59587dab8231dae86abd64
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-06 19:50:20 +0000

    edit betty doc

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- *
+ * print_listint - prints interger
+ * @h: reference structure in header file
+ * Return: return element count
  */
 
 size_t print_listint(const listint_t *h)
