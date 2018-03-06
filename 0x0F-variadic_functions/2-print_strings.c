2c8c5add3d0d196135d468e8af9d587a790553b8
adrielt07 <adriel_tolentino@outlook.com>
2018-03-06 01:52:34 +0000

    fix betty style

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -3,14 +3,17 @@
 #include "stdio.h"
 
 /**
- *
+ * print_strings - prints string then new line
+ * @separator: separator for strings
+ * @n: number of strings
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list valist;
-	va_start(valist ,n);
+
+	va_start(valist, n);
 
 	if (separator != NULL)
 	{
