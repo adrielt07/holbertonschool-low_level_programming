f19a0d34ad0edcaee5cd6fbb215641e46ff53ec0
adrielt07 <adriel_tolentino@outlook.com>
2018-03-07 02:42:58 +0000

    first push

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: address of head
+ * Return: value of head
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *current;
+
+	if (head == NULL)
+		return (n);
+
+	current = *head;
+
+	if (current == NULL)
+		return (n);
+
+	if (current != NULL)
+	{
+		current = current->next;
+		n = current->n;
+		return (n);
+	}
+}
