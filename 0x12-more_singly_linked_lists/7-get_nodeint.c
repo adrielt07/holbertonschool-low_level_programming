a8905fd605ef25f45c61c38e0d811ab8d777367d
adrielt07 <adriel_tolentino@outlook.com>
2018-03-07 02:57:38 +0000

    first push

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return the value of index node
+ * @head: the node
+ * @index: node index to return
+ * Return: value of node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int index_compare = 0;
+
+	while (head != NULL)
+	{
+		if (index_compare == index)
+		{
+			return (head);
+		}
+		index_compare++;
+		head = head->next;
+	}
+
+	return (head);
+}
