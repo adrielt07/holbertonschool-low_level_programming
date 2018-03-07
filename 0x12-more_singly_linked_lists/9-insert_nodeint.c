447d77a68a840bf4271d5a914414430cbc0dd33d
adrielt07 <adriel_tolentino@outlook.com>
2018-03-07 03:38:49 +0000

    assign value of current to new

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->n = n;
 		new->next = current;
+		current = new;
 		return (new);
 	}
 
@@ -45,5 +46,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 	free (new);
-	return (NULL);
+	return (new);
 }
