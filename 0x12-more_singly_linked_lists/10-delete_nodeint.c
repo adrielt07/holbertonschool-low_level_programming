9b9da09478a99c729a3d1db532d1e7c7ca4e8c89
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-08 00:39:30 +0000

    account for if current is NULL

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,8 +35,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			index_compare++;
 			current = current->next;
 		}
-		if (index < index_compare)
-			return (-1);
 	}
+	if (current == NULL)
+		return (-1);
 	return (-1);
 }
