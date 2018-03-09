f9d26d765fe68581fb4a135b1a3360cb74a358bf
adrielt07 <adriel_tolentino@outlook.com>
2018-03-09 02:36:05 +0000

    removed the if statement for if n is NULL

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -8,19 +8,16 @@
 void print_binary(unsigned long int n)
 {
 
-	if (n != NULL)
+	while (n > 1)
 	{
-		while (n > 1)
-		{
-			if (n % 2 == 0)
-				putchar('0');
-			else
-				putchar('1');
-			n /= 2;
-		}
-		if (n == 1)
-			putchar ('1');
+		if (n % 2 == 0)
+			putchar('0');
 		else
-			putchar ('0');
+			putchar('1');
+		n /= 2;
 	}
+	if (n == 1)
+		putchar ('1');
+	else
+		putchar ('0');
 }
