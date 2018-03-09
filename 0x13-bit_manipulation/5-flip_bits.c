3722fbf492b4aa44b7fec55d41851a9a227397d8
adrielt07 <adriel_tolentino@outlook.com>
2018-03-09 06:20:50 +0000

    first push

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - number of flips needed
+ * @n: to compare with m in binary
+ * @m: to compare with n in binary
+ * Return: number of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	int count = 0;
+	int result = 0;
+	unsigned long int compare = n ^ m;
+
+	while (n > 1)
+	{
+		n >>= 1;
+		count++;
+	}
+
+	while (count >= 0)
+	{
+		if (((compare >>count) & 1) == 1)
+			result++;
+		count--;
+	}
+	return(result);
+}
