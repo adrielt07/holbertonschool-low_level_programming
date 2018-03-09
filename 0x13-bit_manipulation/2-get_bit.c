43dcb293c392f7aa1668fb688607adb6f6d959cc
adrielt07 <adriel_tolentino@outlook.com>
2018-03-09 04:22:27 +0000

    accounted for if index is too big

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -13,13 +13,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int count = 0;
 	unsigned long int copy_n = n;
 
-
-
 	if (index == 0)
 	{
 		if (n & 1)
 			return (1);
-		return(0);
+		return (0);
 	}
 
 	if (index > 0)
@@ -31,7 +29,10 @@ int get_bit(unsigned long int n, unsigned int index)
 			count++;
 		}
 
-		if ((n >> count) & 1)
+		if (index > count)
+			return (0);
+
+		else if ((n >> count) & 1)
 			return (1);
 		else
 			return (0);
