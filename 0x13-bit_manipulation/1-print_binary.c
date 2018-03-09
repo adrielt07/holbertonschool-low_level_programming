f77d6337b929517820e739b64866278da879868f
adrielt07 <adriel_tolentino@outlook.com>
2018-03-09 03:25:12 +0000

    account for requirements

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -8,16 +8,27 @@
 void print_binary(unsigned long int n)
 {
 
-	while (n > 1)
+	int count = 0;
+	unsigned long int check;
+	unsigned long int copy_n = n;
+
+	if (n == 0)
+		_putchar('0');
+
+	while (copy_n > 0)
 	{
-		if (n % 2 == 0)
-			putchar('0');
-		else
-			putchar('1');
-		n /= 2;
+		copy_n >>= 1;
+		count++;
 	}
-	if (n == 1)
-		putchar ('1');
-	else
-		putchar ('0');
+	count--;
+
+	while (count >= 0)
+	{
+		check = n >> count;
+		if (check & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+		count--;
+	}
 }
