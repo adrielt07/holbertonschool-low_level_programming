3e213e07fd28eab1cdc523693d80abe67db605f5
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-14 17:54:32 +0000

    fix case if text is null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -33,21 +33,22 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
-	if (fd == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
+		if (fd == -1)
+			return (-1);
 
-	if (text_content == NULL)
-	{
-		writefd = write(fd, "", 0);
-	}
-	else
-	{
 		writefd = write(fd, text_content, len);
 		if (writefd == -1)
 			return (-1);
 	}
-
+	else
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
+		if (fd == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
