1c84c441f60bf67639ae9534b5133d58dc5c0918
adrielt07 <adriel_tolentino@outlook.com>
2018-03-14 04:13:52 +0000

    revert change

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -37,9 +37,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
 	writefd = write(fd, text_content, len);
 	if (writefd == -1)
 		return (-1);
