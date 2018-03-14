da772c93a0737350665a1f85aa3709047ebb478d
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-14 17:17:57 +0000

    account for if text_content is null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -33,6 +33,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (-1);
+
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
 	if (fd == -1)
 		return (-1);
