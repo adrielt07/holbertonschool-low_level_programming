056baea56a42f4b8fd63ce8b51fd44a0f002a3fb
adrielt07 <adriel_tolentino@outlook.com>
2018-03-14 05:37:40 +0000

    fix current user permission

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
 	if (fd == -1)
 		return (-1);
 
