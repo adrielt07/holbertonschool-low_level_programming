61d80ae82538881569b67f1a55b5f35779d6e38f
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-14 17:36:45 +0000

    accounted for if text is NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -34,18 +34,22 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+	}
+	else
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
+		if (fd == -1)
+			return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00400 | 00200);
-	if (fd == -1)
-		return (-1);
-
-	writefd = write(fd, text_content, len);
-	if (writefd == -1)
-		return (-1);
-
+		writefd = write(fd, text_content, len);
+		if (writefd == -1)
+			return (-1);
+	}
 	closefd = close(fd);
-	if (closefd == -1)
-		return (-1);
+
 	return (1);
 }
