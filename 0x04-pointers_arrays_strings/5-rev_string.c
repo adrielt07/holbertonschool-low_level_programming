05383fbae1a9f049b417dbafa67f607c9abe7322
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-30 20:23:53 +0000

    reverse string

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: takes string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int n = 0, i = 0;
+	char *s_copy;
+
+	s_copy = s;
+
+	while(s[n])
+	{
+		n++;
+	}
+	n--;
+
+	while(n >= 0)
+	{
+		s_copy[n] = s[i];
+		n--;
+		i++;
+	}
+}
