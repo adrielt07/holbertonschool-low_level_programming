176e6edd5fdd2790093921c7dbad3faf08341413
Adriel Tolentino <adrieltolentino@outlook.com>
2018-03-30 21:28:39 +0000

    recreate string copy

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,28 @@
 #include "holberton.h"
 
 /**
- *
+ * _strcpy - copies string from src to dest
+ * @dest: string to be copied to
+ * @src: string to copy from
+ * Return: return string dest
  */
 
-char *_strcpy (char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int n;
+	int n = 0, i = 0;
 
-	for (n = 0; src[n] <= '\0'; n++)
+	while (src[n])
 	{
-		dest[n] = src[n];
+		n++;
 	}
+	dest[n] = '\0';
+	n--;
+	while (n >= 0)
+	{
+		dest[i] = src[i];
+		n--;
+		i++;
+	}
+
 	return (dest);
 }
