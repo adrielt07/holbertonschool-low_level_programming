cb2f98af068ab8f82b361530a1089c0bdb3371cc
adrielt07 <adriel_tolentino@outlook.com>
2018-03-31 11:38:29 +0000

    accounted for if string is combination of numeric and alpha

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,29 @@
 
 int _atoi(char *s)
 {
-	int n;
-	int temp = 0;
-	int x;
+	int n = 0;
+	unsigned int temp = 0;
+	unsigned int x;
 
-	for (n = 0; s[n] != '\0'; n++)
+	while (s[n])
 	{
-		if (s[n] >= '0' && s[n] <= '9')
-		{
-			temp = (temp * 10) + (s[n] - '0');
-		}
 		if (s[n] == '-')
 		{
 			x++;
 		}
+		n++;
+	}
+
+	n = 0;
+	while (s[n])
+	{
+		if (s[n] >= '0' && s[n] <= '9')
+		{
+			temp = (temp * 10) + (s[n] - '0');
+			if (s[n + 1] < '0' || s[n + 1] > '9')
+				break;
+		}
+		n++;
 	}
 	if (x % 2 == 1)
 	{
