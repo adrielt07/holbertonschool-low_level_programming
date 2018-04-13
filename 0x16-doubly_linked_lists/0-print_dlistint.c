f79f69be6678fcacb5f0956f1585b7d95d967168
adrielt07 <adriel_tolentino@outlook.com>
2018-04-13 20:23:06 +0000

    fix betty compliant

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_dlistint - prints elements of each node
- * @h: node
+ * @h: pointer to first node
  * Return: number of elements
  */
 
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
