330ee3db0f64ca193c3645aab6cc61b60272e6be
adrielt07 <adriel_tolentino@outlook.com>
2018-04-13 20:43:18 +0000

    first push

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *current = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		new ->next = NULL;
+		*head = new;
+		return (new);
+	}
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	new->next = current->next;
+	current->next = new;
+	new->prev = current;
+	return (new);
+}
