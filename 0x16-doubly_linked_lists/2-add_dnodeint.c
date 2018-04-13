95b76c21ad8c640b9b4a756dc39b2306317439c7
adrielt07 <adriel_tolentino@outlook.com>
2018-04-13 20:23:39 +0000

    first push

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds node at the beginning
+ * @head: new node
+ * @n: value of new node
+ * Return: new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
+}
