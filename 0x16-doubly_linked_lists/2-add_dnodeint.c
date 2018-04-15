bcbe647946f57d1038db4704cf7b31dff04747a1
adrielt07 <adriel_tolentino@outlook.com>
2018-04-15 21:16:19 +0000

    revert changes to first push

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -11,25 +11,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = *head;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	if (*head == NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
-	else
-	{
-		new->n = n;
-		new->next = *head;
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (new);
 }
