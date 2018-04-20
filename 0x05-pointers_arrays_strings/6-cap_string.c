68d54539875911304462c69c5ce2aa11270d66a6
Adriel Tolentino <adrieltolentino@outlook.com>
2018-04-20 23:03:40 +0000

    added two helper functions

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,61 @@
 #include "holberton.h"
 
 /**
+ * convert - converts a char from lowercase to uppser case
+ * @s: takes a char
+ * Return: an uppercase or lowercase
+ */
+char convert(char s)
+{
+	char i = 'a';
+
+	while (i != 'z')
+	{
+		if (i == s)
+			return (i - 32);
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * checker - checks if a character is special
+ * @s: takes a char
+ * Return: 1 if it's a special character, else return 0
+ */
+int checker(char s)
+{
+	char *no = ",;.!?\"(){}\t\n ";
+	int i = 0;
+
+	while (no[i] != '\0')
+	{
+		if (no[i] == s)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
  * *cap_string - capitalize every word
- *
  * @p: takes p strings
- *
  * Return: The value of p
  */
 
 char *cap_string(char *p)
 {
-	int n;
-	int i;
-	char *no = " ,;.!?\"(){}\t\n";
+	int i = 0;
 
-	for (n = 0; p[n] != '\0'; n++)
+	while (p[i] != '\0')
 	{
-		for (i = 0; no[i] != '\0'; i++)
+		if (checker(p[i]) == 1)
 		{
-			if (p[n - 1] == no[i] && (p[n] >= 'a' && p[n] <= 'z'))
-			{
-				p[n] = p[n] - 32;
-			}
+			i++;
+			p[i] = convert(p[i]);
 		}
+		else
+			i++;
 	}
 	return (p);
 }
