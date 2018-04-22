c6f7223fabc377b89da7cd445057e183b4643c86
adrielt07 <adriel_tolentino@outlook.com>
2018-04-22 02:46:10 +0000

    run string len after assigning s1 or s2 to empty string to account if either is NULL

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -30,14 +30,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	unsigned int c;
-	unsigned int l1 = _strlen(s1);
-	unsigned int l2 = _strlen(s2);
+	unsigned int l1;
+	unsigned int l2;
 	unsigned int t = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
 
 	if (n > l2)
 	{
