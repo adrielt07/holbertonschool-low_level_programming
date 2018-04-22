9f364ee5bf4be775fa7a54e4770f7a42be19eae6
adrielt07 <adriel_tolentino@outlook.com>
2018-04-22 02:12:29 +0000

    change the order of statement in while loop and if assign (nil) to string if it's null

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,6 +1,60 @@
 #include "variadic_functions.h"
 
 /**
+ * fmt_c - print character
+ * @valist: takes a character
+ */
+
+void fmt_c(va_list valist)
+{
+	char c;
+
+	c = va_arg(valist, int);
+	printf("%c", c);
+}
+
+/**
+ * fmt_i - prints interger
+ * @valist: take int
+ */
+
+void fmt_i(va_list valist)
+{
+	int i;
+
+	i = va_arg(valist, int);
+	printf("%i", i);
+}
+
+/**
+ * fmt_f - prints float
+ * @valist: takes double
+ */
+
+void fmt_f(va_list valist)
+{
+	double f;
+
+	f = va_arg(valist, double);
+	printf("%f", f);
+}
+
+/**
+ * fmt_s - prints string
+ * @valist: takes string
+ */
+
+void fmt_s(va_list valist)
+{
+	char *s;
+
+	s = va_arg(valist, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
  * print_all - prints string with the correct format
  * @format: format to use
  */
@@ -13,26 +67,19 @@ void print_all(const char * const format, ...)
 		{"i", fmt_i},
 		{"f", fmt_f},
 		{"s", fmt_s},
-		{NULL, NULL},
 	};
 
 	int x, k;
 	char *first;
 	va_list arg_list;
 
-	if (format == NULL)
-	{	printf("nil\n");
-		return;
-	}
-
 	x = 0;
 	first = "";
 	va_start(arg_list, format);
-
-	while (format[x] != '\0' && format != NULL)
+	while (format != NULL && format[x])
 	{
 		k = 0;
-		while (t_format[k].s != NULL)
+		while (k < 4)
 		{
 			if (format[x] == *t_format[k].s)
 			{
