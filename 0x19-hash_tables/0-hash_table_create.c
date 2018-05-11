cd4c9fef537683e7caef6a837ef86cb60fe48962
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-11 23:05:28 +0000

    malloc size of array by pointer of hash_node and size

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,14 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size <= 0)
 		return (NULL);
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		return (NULL);
+	{
+		free(new_table); return (NULL);
+	}
 
 	while (n < size)
 	{
