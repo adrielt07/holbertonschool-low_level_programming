3390fd5bf004b2e75e80b040e4ee680642185d4d
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-11 23:00:29 +0000

    don't free hash_table if array malloc failed

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -20,8 +20,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
+		return (NULL);
+
+	while (n < size)
 	{
-		free(new_table); return (NULL);
+		new_table->array[n] = NULL;
+		n++;
 	}
+
 	return (new_table);
 }
