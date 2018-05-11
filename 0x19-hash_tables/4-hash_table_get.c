e44614c45903a38d9c8ce041f861d31018e1423f
Adriel Tolentino <adrieltolentino@outlook.com>
2018-05-11 23:12:39 +0000

    fix betty compliant

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,17 +11,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 
-	index = key_index((unsigned char*)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	if (ht->array[index] == NULL)
 		return (NULL);
-	while(ht->array[index] != NULL)
+	while (ht->array[index] != NULL)
 	{
-		if(strcmp(ht->array[index]->key, key) >= 0)
+		if (strcmp(ht->array[index]->key, key) >= 0)
 			return (ht->array[index]->value);
-		else
-			ht->array[index] = (ht->array[index])->next;
+		ht->array[index] = (ht->array[index])->next;
 	}
 	return (NULL);
 }
