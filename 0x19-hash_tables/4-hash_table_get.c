40310c04f31321e092e638b7b3157f97c4794350
adrielt07 <adriel_tolentino@outlook.com>
2018-05-12 01:50:35 +0000

    change the if strcmp condition to check if equal 0

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	while (ht->array[index] != NULL)
 	{
-		if (strcmp(ht->array[index]->key, key) >= 0)
+		if (strcmp(ht->array[index]->key, key) == 0)
 			return (ht->array[index]->value);
 		ht->array[index] = (ht->array[index])->next;
 	}
