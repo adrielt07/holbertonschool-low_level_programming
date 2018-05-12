844ca9e1746266a00eb53ddf9baced993acea9a9
adrielt07 <adriel_tolentino@outlook.com>
2018-05-12 01:48:15 +0000

    revert my changes

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -26,7 +26,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(runner->key, key) == 0)
 		{
-			free(runner->value);
 			runner->value = strdup(value);
 			return (1);
 		}
