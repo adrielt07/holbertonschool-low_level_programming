1f3e689a74f4a58e331c94d21c53d84ec5093285
adrielt07 <adriel_tolentino@outlook.com>
2018-05-12 07:52:59 +0000

    return null if ht is null

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new, *runner;
 
-	if (strlen(key) == 0 || key == NULL || value == NULL)
+	if (strlen(key) == 0 || key == NULL || value == NULL || ht == NULL)
 		return (0);
 
 	new = malloc(sizeof(hash_node_t));
