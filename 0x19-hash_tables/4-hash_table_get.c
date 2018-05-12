86587256e57b2d64051e25399fa9cf500e379723
adrielt07 <adriel_tolentino@outlook.com>
2018-05-12 02:14:17 +0000

    pass in a const unsigned char for key_index function

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (NULL);
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
 	while (ht->array[index] != NULL)
