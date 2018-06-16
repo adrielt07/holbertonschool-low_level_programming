eb655a74140885c249e1e0c0b7708b932c404e3d
adrielt07 <adriel_tolentino@outlook.com>
2018-06-16 01:42:57 +0000

    account for len of matrix. Also increment perimeter if it reach max len - 1

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -11,17 +11,17 @@ def island_perimeter(grid):
                 if row != 0:
                     if grid[row - 1][col] == 0:
                         perim += 1
-                if row < len(grid):
+                if row < len(grid) - 1:
                     if grid[row + 1][col] == 0:
                         perim += 1
                 if col != 0:
                     if grid[row][col - 1] == 0:
                         perim += 1
-                if col < len(grid[row]):
+                if col < len(grid[row]) - 1:
                     if grid[row][col + 1] == 0:
                         perim += 1
-                if row == 0:
+                if row == 0 or row == len(grid) - 1:
                     perim += 1
-                if col == 0:
+                if col == 0 or col == len(grid[row]) - 1:
                     perim += 1
     return perim
