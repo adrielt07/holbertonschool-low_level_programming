0aecb5082002e482f0c602102a8b05517813b817
adrielt07 <adriel_tolentino@outlook.com>
2018-06-16 01:32:38 +0000

    initial commit - unfinish

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Finds the perimeter of an island"""
+    perim = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row != 0:
+                    if grid[row - 1][col] == 0:
+                        perim += 1
+                if row < len(grid):
+                    if grid[row + 1][col] == 0:
+                        perim += 1
+                if col != 0:
+                    if grid[row][col - 1] == 0:
+                        perim += 1
+                if col < len(grid[row])):
+                    if grid[row][col + 1] == 0:
+                        perim += 1
+    return perim
