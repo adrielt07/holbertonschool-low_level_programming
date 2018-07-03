f35fad6ab3e4e81be01230ddd3f3ee1e3c3700cf
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-03 20:00:13 +0000

    initial commit

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -10,9 +10,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node != NULL)
 	{
 		if (node->left == NULL && node->right == NULL)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
 
 /**
@@ -23,12 +23,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t count = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_leaf(tree->left) == 0)
-		count = binary_tree_leaves(tree->left);
-	if (binary_tree_is_leaf(tree->right) == 0)
-		count = binary_tree_leaves(tree->right);
-	return (count + 1);
+	if (binary_tree_is_leaf(tree) == 1)
+		return (1);
+	if (tree->left != NULL)
+		count += binary_tree_leaves(tree->left);
+	if (tree->right != NULL)
+		count += binary_tree_leaves(tree->right);
+	return (count);
 }
