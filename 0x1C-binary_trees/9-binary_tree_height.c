51ccd7d96bc2eb99927334e4d1f7618a402f2be9
adrielt07 <adriel_tolentino@outlook.com>
2018-07-03 04:41:35 +0000

    count2 was not being used fixed issue

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -24,17 +24,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t count = 0, count2 = 0;
 
-	if ((tree == NULL) || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_leaf(tree->left) != 1)
+	if (tree->left != NULL)
 	{
-		++count;
+		if (binary_tree_is_leaf(tree->left) != 1)
+			++count;
 		binary_tree_height(tree->left);
-	}
-	if (binary_tree_is_leaf(tree->right) != 1)
-	{
 		++count;
+	}
+	if (tree->right != NULL)
+	{
+		if (binary_tree_is_leaf(tree->right) != 1)
+			++count2;
 		binary_tree_height(tree->right);
+		++count2;
 	}
 	if (count >= count2)
 		return (count);
