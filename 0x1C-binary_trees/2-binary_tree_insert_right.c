97e5157801e915a97aa30edd78b51cb420e21612
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-03 22:08:05 +0000

    fix betty compliant

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_t - creates a binary tree node
+ * binary_tree_insert_right - insert a node at the right of BT
  * @parent: pointer to the parent
  * @value: value of the new node
  * Return: pointer to new node, else NULL
@@ -15,15 +15,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	else
-	{
-		new->n = value;
-		new->left = NULL;
-		new->right = parent->right;
-		if (new->right != NULL)
-			new->right->parent = new;
-		parent->right = new;
-		new->parent = parent;
-		return (new);
-	}
+	new->n = value;
+	new->left = NULL;
+	new->right = parent->right;
+	if (new->right != NULL)
+		new->right->parent = new;
+	parent->right = new;
+	new->parent = parent;
+	return (new);
 }
