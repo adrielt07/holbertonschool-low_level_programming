419e63617064e972fd010cfe4498092c42b7ed1c
adrielt07 <adriel_tolentino@outlook.com>
2018-07-03 05:29:32 +0000

    initiaal commit

---
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measure the size of Binary Tree
+ * @tree: pointer to node to measure depth
+ * Return: depth
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
+}
