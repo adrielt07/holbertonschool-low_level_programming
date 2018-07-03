daca0c819ee1bc01576287e24af587a813778db2
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-03 22:17:57 +0000

    add prototypes for advance

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,14 +10,14 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node != NULL)
 	{
 		if (node->left == NULL && node->right == NULL)
-			return 1;
+			return (1);
 	}
-	return 0;
+	return (0);
 }
 
 /**
- * binary_tree_height - measures the heigh of binary tree
- * @tree: pointer to node to measure height
+ * binary_tree_height - measures the height of binary tree
+ * @tree: pointer to node to measure height from
  * Return: return height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
