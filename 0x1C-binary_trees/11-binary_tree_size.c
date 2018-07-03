455add3025c4109205f1d8194c8fadd568bd9f40
adrielt07 <adriel_tolentino@outlook.com>
2018-07-03 05:32:55 +0000

    change return desc

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - measure the size of Binary Tree
  * @tree: pointer to node to measure depth
- * Return: depth
+ * Return: returns the size of binary
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
