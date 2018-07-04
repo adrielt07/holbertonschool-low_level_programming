df7cf5d5b10779c0e3246845c354ab118c4f6500
adrielt07 <adriel_tolentino@outlook.com>
2018-07-04 04:10:27 +0000

    removed unused functions

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,39 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if node is leaf
- * @node: pointer to node that will be checked
- * Return: 0 if node is null or not a leaf, else 1
- */
-int binary_tree_is_leaf(const binary_tree_t *node)
-{
-	if (node != NULL)
-	{
-		if (node->left == NULL && node->right == NULL)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * binary_tree_height - measures the height of binary tree
- * @tree: pointer to node to measure height from
- * Return: return height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t count = 0, count2 = 0;
-
-	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
-		return (0);
-	count = binary_tree_height(tree->left);
-	count2 = binary_tree_height(tree->right);
-	if (count > count2)
-		return (count + 1);
-	return (count2 + 1);
-}
-
-/**
  * binary_tree_levelorder - prints BT using level-order traversal
  * @tree: pointer to node
  * @func: pointer to function to print
