6dd2ada816d6707184784746d51c7268ef56452a
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-05 08:07:00 +0000

    do nothing if tree or func is null

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -7,6 +7,8 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (tree == NULL || func == NULL)
+		return;
 	if (tree->parent == NULL)
 		func(tree->n);
 	if (tree->left != NULL)
