87246f12bd269ec1dfc727397cb95892e8ef506c
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-05 08:47:53 +0000

    account for if tree is not the top of the tree

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * traverse_print - helps print if tree node is a child
+ * @tree: pointer to node
+ * @func: function to print value
+ * @n: if = 0, print root first one time
+ */
+void traverse_print(const binary_tree_t *tree, void (*func)(int), int n)
+{
+	if (n == 0)
+		func(tree->n);
+	if (tree->left != NULL)
+		func(tree->left->n);
+	if (tree->right != NULL)
+		func(tree->right->n);
+	if (tree->left != NULL)
+		help(tree->left, func, 1);
+	if (tree->right != NULL)
+		help(tree->right, func, 1);
+}
+
+/**
  * binary_tree_levelorder - prints BT using level-order traversal
  * @tree: pointer to node
  * @func: pointer to function to print
@@ -9,14 +29,5 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-	if (tree->parent == NULL)
-		func(tree->n);
-	if (tree->left != NULL)
-		func(tree->left->n);
-	if (tree->right != NULL)
-		func(tree->right->n);
-	if (tree->left != NULL)
-		binary_tree_levelorder(tree->left, func);
-	if (tree->right != NULL)
-		binary_tree_levelorder(tree->right, func);
+	help(tree, func, 0);
 }
