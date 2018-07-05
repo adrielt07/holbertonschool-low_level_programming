72aca0ba2a5897a5f730987745e7af2e5e492514
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-05 08:00:05 +0000

    checks if all parent has 0 or two childs, else it's not full

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * child_check - checks if a node only has one child
+
+ */
+int child_check(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL && node->right == NULL)
+		return (1);
+	if (node->right != NULL && node->left == NULL)
+		return (1);
+	return (0);
+}
+
+/**
  * binary_tree_is_leaf - checks if node is leaf
  * @node: pointer to node that will be checked
  * Return: 0 if node is null or not a leaf, else 1
@@ -16,78 +31,25 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 }
 
 /**
- * binary_tree_height - measures the height of binary tree
- * @tree: pointer to node to measure height from
- * Return: return height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t count = 0, count2 = 0;
-
-	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
-		return (0);
-	count = binary_tree_height(tree->left);
-	count2 = binary_tree_height(tree->right);
-	if (count > count2)
-		return (count + 1);
-	return (count2 + 1);
-}
-
-/**
- * max_elem_calc - calculates the maximum posible number of node in binary tree
- * at certain level
- * @height: takes height of binary tree
- * Return: number of possible nodes
- */
-int max_elem_calc(int height)
-{
-	int count = 0, first_calc = 1;
-
-	while (height > 0)
-	{
-		if (first_calc == 1)
-			count += 1 * 2;
-		else
-			count += count * 2;
-		first_calc = 0;
-		height--;
-	}
-	if (count > 1)
-		count++;
-	return (count);
-}
-
-/**
- * binary_tree_size - measure the size of Binary Tree
- * @tree: pointer to node to check from
- * Return: returns the size of binary
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (1 + binary_tree_size(tree->left) +
-		binary_tree_size(tree->right));
-}
-
-/**
  * binary_tree_is_full - checks if node and its sub trees are full
  * @tree: pointer to root node
  * Return: 1 if full, else 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t height = 0, max_elem = 0;
-
+	int left = 0, right = 0;
 	if (tree == NULL)
 		return (0);
-
-	height = binary_tree_height(tree);
-	if (height == 0)
-		return (1);
-	max_elem = max_elem_calc(height);
-	if (max_elem == binary_tree_size(tree))
-		return (1);
-	return (0);
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+	if (binary_tree_is_leaf(tree) != 0)
+	{
+		if (child_check(tree) == 1)
+			return (0);
+		else
+			return (1);
+	}
+	if (left == 0 || right == 0)
+		return (0);
+	return (1);
 }
