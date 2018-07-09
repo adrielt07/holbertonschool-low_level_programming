dc6e35bf4ff0681c34fa5644c6e0ccd92310d93a
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-09 23:06:16 +0000

    add prototype for task 100

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,26 +1,17 @@
 #ifndef _Holberton1
 #define _Holberton1
-
-/**
- *
- */
+#include "stdlib.h"
 
 char *_strcat(char *dest, char *src);
-
 char *_strncat(char *dest, char *src, int n);
-
 char *_strncpy(char *dest, char *src, int n);
-
 int _strcmp(char *s1, char *s2);
-
 void reverse_array(int *a, int n);
-
 char *string_toupper(char *);
-
 char *cap_string(char *);
-
 char *leet(char *);
-
 char *rot13(char *);
+int _putchar(char c);
+void print_number(int n);
 
-#endif
+#endif /* _Holberton1 */
