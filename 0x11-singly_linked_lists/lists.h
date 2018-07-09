e7a3eaddc7487ac73ae1b0fb45a617c6968c29b8
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-09 20:56:05 +0000

    added stdio.h and fix comments

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -3,10 +3,6 @@
 #include <stddef.h>
 
 /**
- * Prototypes & Struct
- */
-
-/**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
@@ -26,5 +22,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void) __attribute__ ((constructor));
 
-#endif
+#endif /* _linkedList_ */
