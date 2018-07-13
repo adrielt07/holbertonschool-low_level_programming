23aa19564fe1ed00a25832de13a533c9d00121fb
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-13 00:25:06 +0000

    added a helper function to determine if n is prime number

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ * helper_prime - helps to find if n is prime
+ * @n: to be checked if prime
+ * @i: checker that determines if n is prime
+ * Return: 1 if prime, else 0
+ */
+int helper_prime(int n, int i)
+{
+	if (i == n)
+		return (1);
+	if (i * i == n)
+		return (0);
+	return (helper_prime(n, i + 1));
+}
+
+/**
  * is_prime_number - checks if number is a prime numer
  * @n: takes an interger
  * Return: 1 if prime, else 0
@@ -8,25 +23,9 @@
 
 int is_prime_number(int n)
 {
-	if (n < 0)
-	{
-		return (is_prime_number(n * -1));
-	}
-
 	if (n == 1)
-	{
 		return (0);
-	}
-	if (n == 2 || n == 3 || n == 5 || n == 7)
-	{
-		return (1);
-	}
-	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0 || n % 9 == 0)
-	{
+	if (n < 0)
 		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	return (helper_prime(n, 2));
 }
