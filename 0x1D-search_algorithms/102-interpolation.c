deb672c3ae844db5301d5aa1709bba250816c5fd
adrielt07 <adrieltolentino@outlook.com>
2018-08-13 06:47:39 +0000

    initial commit

---
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - uses interpolation search algorithm to find value
+ * @array: pointer to the array to search in
+ * @size: number of elements in the array
+ * @value: value to look for in the array
+ * Return: Index location of value, else -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t pos;
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			     * (value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
